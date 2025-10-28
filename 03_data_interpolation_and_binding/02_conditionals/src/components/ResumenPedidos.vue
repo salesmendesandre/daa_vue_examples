8f0c8f3ab7a884cc1afa60175500255b53ba97e3
André Sales Mendes <salesmendesandre@gmail.com>
2025-10-28 17:09:57 +0100

    Cap 3

---
+++ 03_data_interpolation_and_binding/02_conditionals/src/components/ResumenPedidos.vue
@@ -0,0 +1,16 @@
+<template>
+  <dl>
+    <template v-for="(valor, clave) in resumen" :key="clave">
+      <dt>{{ clave }}</dt>
+      <dd>{{ valor }}</dd>
+    </template>
+  </dl>
+</template>
+
+<script setup>
+const resumen = {
+  'Pedidos activos': 7,
+  'Pedidos completados': 24,
+  'Pendientes de envío': 3
+}
+</script>
