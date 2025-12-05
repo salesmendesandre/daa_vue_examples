9b1831259dd3399ae14de88ce09de13e02a37ebf
André Sales Mendes <salesmendesandre@gmail.com>
2025-12-05 09:50:12 +0100

    Act

--- 19_upload_files/02_client/src/router/index.js
+++ 19_upload_files/02_client/src/router/index.js
@@ -3,8 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: () => import('../views/HomeView.vue'),
+    name: 'UploadImage',
+    component: () => import('../views/UploadImage.vue'),
   },
   {
     path: '/about',
